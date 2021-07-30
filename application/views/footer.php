b540d8bb0e061ceafe4d7f0b8b5e5fc6e35d7140
MuhammadFikri <fikririyadi5@gmail.com>
2021-07-30 15:10:12 +0700

    egt

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+				<section id="footer" class="page-section">
+					<footer id="footer-section" class="page-section pt-30 pb-30 center-xs">
+						<div class="copyright fw300 mb-sm-10"><span>&copy; EGT <?php echo date('Y'); ?> </span></div>
+						<!-- SCROLL TO TOP-->
+						<div class="top-button">
+							<h5>Top</h5>
+							<div class="line"></div>
+						</div>
+					</footer>
+				</section>
